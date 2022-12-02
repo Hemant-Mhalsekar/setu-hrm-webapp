839510d003d9454094440107539b6d9eee5a3b6b
abhisheksmandal <abhishek.s.mandal@gmail.com>
2022-12-02 17:57:04 +0530

    Added Navbar and Modules

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/header";
+// import Logo from "./components/imgs/HDSOFT logo.png";
+import Modules from "./components/modules";
 
 function App() {
   return (
-    <Header/>
+    <>
+      <Header />
+      <Modules />
+    </>
   );
 }
 
---
+++ src/components/modules.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+export default function modules() {
+  return (
+    <div className="moduleContainer">
+      <div className="list-group">
+        <a href="#" className="list-group-item list-group-item-action clicked">
+          Home
+        </a>
+        <a href="#" className="list-group-item list-group-item-action">
+          Projects
+        </a>
+        <a href="#" className="list-group-item list-group-item-action">
+          Employee Registration
+        </a>
+        <a href="#" className="list-group-item list-group-item-action">
+          Project Allocation
+        </a>
+        <a href="#" className="list-group-item list-group-item-action">
+          Task Allocation
+        </a>
+        <a href="#" className="list-group-item list-group-item-action">
+          Employees
+        </a>
+        <a href="#" className="list-group-item list-group-item-action">
+          Leave Master
+        </a>
+        <a href="#" className="list-group-item list-group-item-action">
+          Reports
+        </a>
+      </div>
+    </div>
+  );
+}
